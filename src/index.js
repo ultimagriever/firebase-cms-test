40d80ee97acc28f5bb33ec50bb6dbdc52a6c4c08
Pamela Argentino <argentinopam@hotmail.com>
2017-07-29 18:14:49 -0300

    Configure Firebase and Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers'
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={createStore(reducer)}>
+  <App />
+</Provider>, document.getElementById('root'));
